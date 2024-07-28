f8a6781f75e0091e98fb55ea6a6f6af793b3b065
tb <travisbikkle@proton.me>
2024-07-28 22:03:36 +0800

    update rust

--- update.ps1
+++ update.ps1
@@ -81,6 +81,9 @@ Get-ChildItem -Recurse -Path .\bucket | ForEach-Object -Process {
     # Go
     (Get-Content $_.FullName) -replace 'dl\.google\.com/go', 'mirrors.aliyun.com/golang' | Set-Content -Path $_.FullName
 
+    # rust
+    (Get-Content $_.FullName) -replace 'static\.rust-lang\.org', 'mirrors.tuna.tsinghua.edu.cn/rustup' | Set-Content -Path $_.FullName
+
     # VLC
     (Get-Content $_.FullName) -replace 'download\.videolan\.org/pub', 'mirrors.aliyun.com/videolan' | Set-Content -Path $_.FullName
 
